21235420b0141ab2005e690110c0f529b96b7e8a
Denis <deshatyrko@yandex.ru>
2025-06-29 19:50:36 +0300

    Feat: delete

--- .idea/src/main.ts
+++ .idea/src/main.ts
@@ -11,6 +11,11 @@ server.post("/", (req: express.Request, res: express.Response) => {
   res.json ({massage:'Спасибо за POST запрос'})
 });
 
+server.delete("/", (req: express.Request, res: express.Response) => {
+  console.log('Удаление задачи');
+  res.json ({massage:'Удаление не поддерживается'})
+});
+
 const port = 2000;
 
 server.listen(port, () =>{
