ae34b06a0a61c44a48f1dac7042d00ad4c648027
Denis <deshatyrko@yandex.ru>
2025-07-02 18:27:25 +0300

    Feat: Добавил Eslint

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import express from "express";
+import express from 'express';
+
 const server = express();
 
-server.get("/", (req: express.Request, res: express.Response) => {
+server.get('/', (req: express.Request, res: express.Response) => {
   console.log('Пришел запрос GET /');
-  res.json ({massage:'Спасибо за GET запрос'})
+  res.json({ massage: 'Спасибо за GET запрос' });
 });
 
-server.post("/", (req: express.Request, res: express.Response) => {
+server.post('/', (req: express.Request, res: express.Response) => {
   console.log('Пришел запрос POST /');
-  res.json ({massage:'Спасибо за POST запрос'})
+  res.json({ massage: 'Спасибо за POST запрос' });
 });
 
-server.delete("/", (req: express.Request, res: express.Response) => {
+server.delete('/', (req: express.Request, res: express.Response) => {
   console.log('Удаление задачи');
-  res.json ({massage:'Удаление не поддерживается'})
+  res.json({ massage: 'Удаление не поддерживается' });
 });
 
 const port = 2000;
 
-server.listen(port, () =>{
-  console.log( `Listening on port ${port}` );
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
-
