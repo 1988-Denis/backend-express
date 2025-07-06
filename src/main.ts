78bfbc3402111fb271b1c9fa73c6ec04d408e662
Denis <deshatyrko@yandex.ru>
2025-07-06 16:01:33 +0300

    Feat: Подправил импорты типов Request, Response

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
 
-server.get('/', (req: express.Request, res: express.Response) => {
+server.get('/', (req: Request, res: Response) => {
   console.log('Пришел запрос GET /');
   res.json({ massage: 'Спасибо за GET запрос' });
 });
 
-server.post('/', (req: express.Request, res: express.Response) => {
+server.post('/', (req: Request, res: Response) => {
   console.log('Пришел запрос POST /');
   res.json({ massage: 'Спасибо за POST запрос' });
 });
 
-server.delete('/', (req: express.Request, res: express.Response) => {
+server.delete('/', (req: Request, res: Response) => {
   console.log('Удаление задачи');
   res.json({ massage: 'Удаление не поддерживается' });
 });
